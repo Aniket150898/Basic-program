43ab9b7c4dd910c4292c39107cc4c7624feb20d0
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-09 17:49:31 +0530

    Power of two

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Powerof2
+{
+    internal class Power
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value :");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N > 0 && N < 31)
+            {
+                for (int i = 0; i <= N; i++)
+                    Console.WriteLine(2 + " ^ " + i + " = " + Math.Pow(2, i));
+            }
+            else
+            {
+                Console.WriteLine("Enter No in range 0 to 30");
+            }
+        }
+    }
+}
