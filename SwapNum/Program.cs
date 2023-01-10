8161921281fda5f3e8c070b43b1d20585fe9581e
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-10 12:00:04 +0530

    {Aniket}-Swap the number

---
+++ SwapNum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SwapNum
+{
+    internal class SwapNumber
+    {
+        static void Main(String[] args)
+        {
+
+            int temp;
+            Console.WriteLine("Enter the first number :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+
+            Console.WriteLine("After Swap num1 = " + num1);
+            Console.WriteLine("After Swap num2 = " + num2);
+        }
+    }
+}
