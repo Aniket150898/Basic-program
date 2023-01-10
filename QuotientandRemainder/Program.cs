303fcbaf97c8f80d90afea3e09ba3eb2ca24645e
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-10 11:57:05 +0530

    {Aniket}-Add Quotient & Remainder

---
+++ QuotientandRemainder/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace QuotientandRemainder
+{
+    internal class QuotientRemainder
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, quotient, remainder;
+
+            Console.WriteLine("Enter the first number : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            quotient = num1 / num2;
+            remainder = num1 % num2;
+
+            Console.WriteLine("Quotient is num1 is divide by num2 : " + quotient);
+            Console.WriteLine("Remainder is num1 is divide by num2 : " + remainder);
+        }
+    }
+}
