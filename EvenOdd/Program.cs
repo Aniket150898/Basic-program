fb12d2542d150b6b6070903f816e15409c3e8fdb
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-10 12:02:28 +0530

    {Aniket} even or add number

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EvenOdd
+{
+    internal class EvenOdd
+    {
+        static void Main(string[]arg)
+        {
+            Console.WriteLine(" Enter a number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " is an Even number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is an Odd number");
+            }
+        }
+    }
+}
