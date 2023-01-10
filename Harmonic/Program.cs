b4d7153ae8577e27802e32c800d353c4b82ce329
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-10 11:49:07 +0530

    Harmonic program Add

---
+++ Harmonic/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Harmonic
+{
+    internal class Harmonic
+    {
+        static void Main(string[] args)
+        {
+            double num = 0, result = 0, i;
+
+            Console.WriteLine(" Enter a number ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                num = num + (1 / i);
+                result = num;
+            }
+            Console.WriteLine("The value of harmonic no. " + n + " is: " + result);
+        }
+    }
+}
